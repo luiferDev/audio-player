dc399313a01b9492b305370480199283502b4873
Jorge Morales <luifer991@gmail.com>
2024-07-16 20:17:29 -0500

    adding forward and backward button funcionatiluy of changing music TODO: making audio play and pause according to the section

--- main.js
+++ main.js
@@ -3,20 +3,13 @@ const btnLeft = document.querySelector(".btn-left"),
   slider = document.querySelector("#slider"),
   sliderSection = document.querySelectorAll(".slider-section"),
   btnPlay = document.querySelector(".play-btn"),
-  audio = document.querySelector(".player__audio")
+  audio1 = document.querySelector("#audio1"),
+  audio2 = document.querySelector("#audio2")
 
 btnLeft.addEventListener("click", e => moveToLeft())
 btnRight.addEventListener("click", e => moveToRigth())
-btnPlay.addEventListener("click", e => playAudio())
+//btnPlay.addEventListener("click", e => playAudio())
 // btnPlay.addEventListener("click", e => pauseAudio())
-
-function playAudio() {
-  if (audio.paused) {
-    audio.play();
-  } else {
-    audio.pause();
-  }
-}
 
 // function pauseAudio() {
 //   if (audio == audio.play()) return
@@ -28,19 +21,34 @@ let operator = 0,
   position = 0,
   widthSections = 100 / sliderSection.length
 
+  console.log(sliderSection.length)
+  console.log(widthSections)
 function moveToRigth() {
   if (position >= sliderSection.length - 1) {
     operator = 0
     position = 0
     slider.style.transform = `translateX(-${operator}%)`
     slider.style.transition = "none"
+    audio1.play()
+    audio2.pause()
+    audio2.currentTime = 0
+    btnPlay.addEventListener("click", e => {
+      if (audio1.play()) {
+        audio1.pause()
+      }})
     return
   }
   position++
   operator = operator + widthSections
   slider.style.transform = `translateX(-${operator}%)`
   slider.style.transition = "all ease-in .5s"
-
+  audio2.play()
+  audio1.pause()
+  audio1.currentTime = 0
+  btnPlay.addEventListener("click", e => {
+    if (audio2.play()) {
+      audio2.pause()
+    }})
 }
 
 function moveToLeft() {
@@ -50,10 +58,23 @@ function moveToLeft() {
     position = sliderSection.length - 1
     slider.style.transform = `translateX(-${operator}%)`
     slider.style.transition = "none"
+    audio2.play()
+    audio1.pause()
+    audio1.currentTime = 0
+    btnPlay.addEventListener("click", e => {
+      if (audio2.play()) {
+        audio2.pause()
+      }})
     return
   }
   operator = operator - widthSections
   slider.style.transform = `translateX(-${operator}%)`
   slider.style.transition = "all ease .5s"
-
+  audio1.play()
+  audio2.pause()
+  audio2.currentTime = 0
+  btnPlay.addEventListener("click", e => {
+    if (audio1.play()) {
+      audio1.pause()
+    }})
 }
